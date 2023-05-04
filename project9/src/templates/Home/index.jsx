35903a119e8cb280cf5405d215444e0c72bcae1e
pedro henrique <pedrouniessa881@gmail.com>
2023-05-04 14:14:11 -0300

    Compound Components com useContext

---
+++ project9/src/templates/Home/index.jsx
@@ -0,0 +1,54 @@
+// Compound Components
+import { createContext, useContext, useState } from 'react';
+
+const s = {
+  style: {
+    fontSize: '60px',
+  },
+};
+
+const TurnOnOffContext = createContext();
+
+const TurnOnOff = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+  const onTurn = () => setIsOn((s) => !s);
+
+  return <TurnOnOffContext.Provider value={{ isOn, onTurn }}>{children}</TurnOnOffContext.Provider>;
+};
+
+const TurnedOn = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? children : null;
+};
+const TurnedOff = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? null : children;
+};
+
+const TurnButton = ({ ...props }) => {
+  const { isOn, onTurn } = useContext(TurnOnOffContext);
+  return (
+    <button onClick={onTurn} {...props}>
+      Turn {isOn ? 'OFF' : 'ON'}
+    </button>
+  );
+};
+
+const P = ({ children }) => <p {...s}>{children}</p>;
+
+export const Home = () => {
+  return (
+    <TurnOnOff>
+      <div>
+        <p>oi</p>
+        <TurnedOn>
+          <P>Aqui as coisas que vão acontecer quando estiver ON.</P>
+        </TurnedOn>
+        <TurnedOff>
+          <P>Aqui vem as coisas do OFF.</P>
+        </TurnedOff>
+        <TurnButton {...s} />
+      </div>
+    </TurnOnOff>
+  );
+};
