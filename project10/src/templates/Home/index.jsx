7d91ddff5efa0cf1a3f51f0cf49c88c0a58c0010
pedro henrique <pedrouniessa881@gmail.com>
2023-05-04 15:26:27 -0300

    React.lazy & CodeSpliting

---
+++ project10/src/templates/Home/index.jsx
@@ -0,0 +1,23 @@
+import React, { Suspense, useState } from 'react';
+//import LazyComponent from './lazy-component';
+
+const loadComponent = () => {
+  console.log('Loading component');
+  return import('./lazy-component');
+};
+const LazyComponent = React.lazy(loadComponent);
+
+export const Home = () => {
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <p>
+        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
+          Show {show ? 'LC on screen' : 'LC is off screen'}
+        </button>
+      </p>
+      <Suspense fallback={<p>Carregando</p>}>{show && <LazyComponent />}</Suspense>
+    </div>
+  );
+};
