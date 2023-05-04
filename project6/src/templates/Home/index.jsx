6331694721d9660112bf67e5ab31a9d8621d2e60
pedro henrique <pedrouniessa881@gmail.com>
2023-05-04 08:48:20 -0300

    useDebugValue & useMediaQuery

--- project6/src/templates/Home/index.jsx
+++ project6/src/templates/Home/index.jsx
@@ -1,3 +1,38 @@
+import { useDebugValue, useEffect, useState } from 'react';
+
+const useMediaQuery = (queryValue, initialValue = false) => {
+  const [match, setMatch] = useState(false);
+
+  useDebugValue(`Query: ${queryValue}`, (name) => {
+    return name + 'modificado';
+  });
+
+  useEffect(() => {
+    let isMounted = true;
+    const matchMedia = window.matchMedia(queryValue);
+
+    const handleChange = () => {
+      if (!isMounted) return;
+      setMatch(Boolean(matchMedia.matches));
+    };
+
+    matchMedia.addEventListener('change', handleChange);
+    setMatch(!!matchMedia.matches);
+
+    return () => {
+      isMounted = false;
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [queryValue]);
+
+  return match;
+};
+
 export const Home = () => {
-  return <h1>oi</h1>;
+  const huge = useMediaQuery('(min-width: 980px)');
+  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
+  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)');
+  const small = useMediaQuery('(max-width: 321px');
+  const background = huge ? 'green' : big ? 'red' : medium ? 'yellow' : small ? 'purple' : null;
+  return <h1 style={{ fontSize: '60px', background: background }}>oi</h1>;
 };
